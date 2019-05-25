a7ec5a4869ff358a4026a84c00eac73cee4f250b
Marcelo <martimiano.marcelo@gmail.com>
2019-05-24 22:36:46 -0300

    código comentado

--- src/main.c
+++ src/main.c
@@ -9,68 +9,76 @@ int is_prime_number(uint64_t number);
 void get_inputs(uint64_t inputs[], int* number_of_inputs);
 void* thread_routine(void *arg);
 
-//100000007 100000217 100000399 100003927 100000421
-pthread_mutex_t  input_lock;
+// Protects the work distribution
+pthread_mutex_t input_lock; 
 
-pthread_mutex_t  prime_lock; // responsável por proteger o contador de primos
+// Protects the primes counter
+pthread_mutex_t prime_lock; 
 
+// Array of the ids of threads
 pthread_t threads_id[MAX_THREAD_NUMBER];
 
+// Counter of the number of primes
 int number_of_primes = 0;
 
+// Input management
 uint64_t inputs[30];
 int number_of_inputs;
 int input_index = 0;
-
-
 
 void* thread_routine(void *arg) {
     
     int work_is_ended = 0;
     uint64_t number;
-    int thread = *((int*) arg);
 
+    // Repeats while there are inputs to be tested
     while (!work_is_ended) {
+        // Wait until no thread is using the inputs array
         pthread_mutex_lock(&input_lock);
-        
+
+        // If there are still inputs to be tested
         if (input_index < number_of_inputs) {
-           // printf("thread %d pegou index: %lu\n", thread, inputs[input_index]);
+
+            // Get the number and move the index
             number = inputs[input_index];
             input_index++;
+            
+            // Free the input_lock
             pthread_mutex_unlock(&input_lock);
             
-            //printf("testando: %lu\n", number);
+            // Verify whether the number is prime
             if (is_prime_number(number)) {
-             //   printf("%lu é primo\n", number);
+                
+                // Update the counter avoinding the race condition
                 pthread_mutex_lock(&prime_lock);
                 number_of_primes++;
                 pthread_mutex_unlock(&prime_lock);
             }
         } else {
+
             work_is_ended = 1;
+            // Free the lock of inputs
             pthread_mutex_unlock(&input_lock);
         }
     }
-//    printf("sai do while\n");
 }
 
 int main() {
 
-
-
-    
-        int index[] = {1,2,3,4};
     // Get the numbers and save them in inputs array
     get_inputs(inputs, &number_of_inputs);
     
+    // Crete all the threads
     for (int i = 0; i < MAX_THREAD_NUMBER; i++) {
-        pthread_create(&threads_id[i], NULL, thread_routine, &index[i]);
+        pthread_create(&threads_id[i], NULL, thread_routine, NULL);
     }
 
-    /* Esperando threads terminarem */
+    // Wait until all the threads end
     for (int i = 0; i < MAX_THREAD_NUMBER; i++) {
         pthread_join(threads_id[i], NULL);
     }
+    
+    // Print the result
     printf("%d\n", number_of_primes);
 
     return 0;
